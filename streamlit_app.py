c16e73de67cec63c13f8284ce95e55234778508b
maryums143-cmyk <maryums143@gmail.com>
2026-06-30 12:23:47 +0400

    Update streamlit_app.py
    Put the JSON into a Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,5 @@ if ingredient_list:
 #Added section to display smoothiefront nutrition information
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
