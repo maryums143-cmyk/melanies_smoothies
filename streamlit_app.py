a577262f4d8dc18e5807ac0c5f237ae0ea444ac4
maryums143-cmyk <maryums143@gmail.com>
2026-06-30 12:22:08 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ if ingredient_list:
 #Added section to display smoothiefront nutrition information
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
